2a65ab92db94d2b4fbef02b40f6f0e7d038c57ce
sreerag <sreerag.sajeesh@tarento.com>
2026-08-07 13:13:59 +0530

    KAR-1298 : Regex fix

--- src/main/java/org/sunbird/common/util/ProjectUtil.java
+++ src/main/java/org/sunbird/common/util/ProjectUtil.java
@@ -211,7 +211,7 @@ public class ProjectUtil {
 	}
 
 	public static Boolean validateRegexPatternWithSpecialCharacter(String regex) {
-		return regex.matches("^[a-zA-Z0-9 \\-()&/]*$");
+		return regex.matches("^[a-zA-Z0-9 \\-()&/,]*$");
 	}
 
 	public static Boolean validatePinCode(String regex) {
